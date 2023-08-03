827039bb0bbe5cf32b92b164023783b42e4a3e66
IDGS-802-20001492 <20000559@alumnos.utleon.edu.mx>
2023-08-02 18:07:12 -0600

    Cambios Merge ClientesDirecciones

--- muebleriaValadezBack/muebleriaValadezBack/Models/ClienteDireccion.cs
+++ muebleriaValadezBack/muebleriaValadezBack/Models/ClienteDireccion.cs
@@ -1,6 +1,6 @@
 ﻿namespace muebleriaValadezBack.Models
 {
-    public class ClienteDireccion
+    public class ClienteDirecciones
     {
         public long idCliente_Direccion { get; set; }
         public long idCliente { get; set; }
